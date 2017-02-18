20cffc0a8b46671fda31056e31a6a2e911ea3ffb
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2017-02-18 17:52:18 -0500

    sketch how to update listing

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# this is a sketch of how to update list of planets
+
+which csvkit || {
+    sudo pip install csvkit
+}
+
+which jq || {
+    sudo apt install jq
+}
+
+if [ ! -e exoplanet.eu_catalog.csv ]; then
+    wget http://exoplanet.eu/catalog/csv/ -O exoplanet.eu_catalog.csv
+fi
+
+csvjson exoplanet.eu_catalog.csv | jq . > data/exoplanet.json
